b239b5b8c49675707825d2ed7adfdf820a276d76
Michaela Maclean <72842715+mickey-maclean@users.noreply.github.com>
2022-08-13 16:08:06 -0300

    Fixed unathorized user routing

--- src/Routers.js
+++ src/Routers.js
@@ -1,30 +1,29 @@
-import { useContext } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom"
-import { AuthContext } from "./context/AuthContext";
+import { AuthProvider } from "./context/AuthContext";
 import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 import NotFound from "./pages/notFound/NotFound";
 import Signup from "./pages/signup/Signup";
+import PrivateRoute from "./utils/PrivateRoute";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const Routers = () => {
-  const {token} = useContext(AuthContext);
-
   return (
     <BrowserRouter>
-        <Routes>
-          {
-            token ? 
-              <>
-                <Route path='/' element={<Home/>} />
-              </>
-            :
-              <>
+        <AuthProvider>
+           <ToastContainer/>
+            <Routes>
+                <Route element={<PrivateRoute/>}>
+                    <Route path='/' element={<Home/>} />
+                </Route>
+            
                 <Route path='/login' element={<Login/>} />
                 <Route path='/cadastrar' element={<Signup/>} />
-              </>
-          }
-          <Route path='/*' element={<NotFound/>} />
-        </Routes>
+                <Route path='/*' element={<NotFound/>} />
+            </Routes> 
+        </AuthProvider>
+        
     </BrowserRouter>
   )
 }
--- src/index.js
+++ src/index.js
@@ -1,16 +1,10 @@
 import React from 'react';
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
 import ReactDOM from 'react-dom/client';
-import { AuthProvider } from './context/AuthContext';
 import './index.css';
 import Routers from './Routers';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <AuthProvider>
-        <ToastContainer/>
-        <Routers/>
-    </AuthProvider>
+root.render(  
+    <Routers/>
 );
 
